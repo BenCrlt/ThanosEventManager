4db9646c53ce633ef82cc682a4dfe0a3f07b0192
Jimmy Narond <73819150+manco6@users.noreply.github.com>
2020-12-15 16:26:14 +0100

    Classes Java :  Event Groupe et Membre

---
+++ ThanosEventManager/app/src/main/java/com/example/thanoseventmanager/classes/Membre.java
@@ -0,0 +1,14 @@
+package com.example.thanoseventmanager.classes;
+
+public class Membre {
+
+    String numero;
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
