81e18b174c5d8401d57201b085549fcd9f7251f0
BenCrlt <benoitcournault@gmail.com>
2020-12-18 11:54:14 +0100

    Create Firebase project

---
+++ ThanosEventManager/app/src/main/java/com/example/thanoseventmanager/api/EventHelper.java
@@ -0,0 +1,5 @@
+package com.example.thanoseventmanager.api;
+
+public class EventHelper {
+    private static final String COLLECTION_NAME = "events";
+}
---
+++ ThanosEventManager/app/src/main/java/com/example/thanoseventmanager/api/MembreHelper.java
@@ -0,0 +1,4 @@
+package com.example.thanoseventmanager.api;
+
+public class MembreHelper {
+}
