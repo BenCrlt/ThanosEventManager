8105120a7d03ca762397ea70d908225bb33ebec0
Jimmy Narond <73819150+manco6@users.noreply.github.com>
2020-12-18 10:08:08 +0100

    impl menu @mainAfterLogin

--- ThanosEventManager/app/src/main/java/com/example/thanoseventmanager/MainAfterLogin.java
+++ ThanosEventManager/app/src/main/java/com/example/thanoseventmanager/MainAfterLogin.java
@@ -3,6 +3,8 @@ package com.example.thanoseventmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainAfterLogin extends AppCompatActivity {
 
@@ -11,4 +13,14 @@ public class MainAfterLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_after_login);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        //R.menu.menu est l'id de notre menu
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
 }
