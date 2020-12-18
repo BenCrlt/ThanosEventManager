0e9598e89a62110cecf15818bc8d6780c77980e7
Jimmy Narond <73819150+manco6@users.noreply.github.com>
2020-12-18 11:39:15 +0100

    Impl listView + modele "Lieu"

--- ThanosEventManager/app/src/main/java/com/example/thanoseventmanager/MainAfterLogin.java
+++ ThanosEventManager/app/src/main/java/com/example/thanoseventmanager/MainAfterLogin.java
@@ -5,6 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.thanoseventmanager.modeles.Event;
+import com.example.thanoseventmanager.modeles.Groupe;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class MainAfterLogin extends AppCompatActivity {
 
@@ -12,6 +21,29 @@ public class MainAfterLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_after_login);
+
+        //Création d'un objet ListView correspondant à "listEvents" du layout activity_main_after_login.xml
+        ListView listEvents = (ListView)findViewById(R.id.listEvents);
+
+        //Pour tester ListView : je créer quelques events
+        Event event1 = new Event("1","event1", new Date(), new Groupe());
+        Event event2 = new Event("2","event2", new Date(), new Groupe());
+        Event event3 = new Event("3","event3", new Date(), new Groupe());
+
+        //On les ajoute à une liste d'évènements
+        List<Event> events = new ArrayList<Event>();
+
+        events.add(event1);
+        events.add(event2);
+        events.add(event3);
+
+        // android.R.layout.simple_list_item_1 is a constant predefined layout of Android.
+        // used to create a ListView with simple ListItem (Only one TextView).
+
+        ArrayAdapter<Event> arrayAdapter
+                = new ArrayAdapter<Event>(this, android.R.layout.simple_list_item_1 , events);
+
+        listEvents.setAdapter(arrayAdapter);
     }
 
     @Override
---
+++ ThanosEventManager/app/src/main/java/com/example/thanoseventmanager/modeles/Lieu.java
@@ -0,0 +1,50 @@
+package com.example.thanoseventmanager.modeles;
+
+public class Lieu {
+
+    String id;
+    String nom;
+    String adresse;
+    String ville;
+    String cp;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getCp() {
+        return cp;
+    }
+
+    public void setCp(String cp) {
+        this.cp = cp;
+    }
+}
